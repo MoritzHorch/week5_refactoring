49349168347e7b9386940f5310a3394be5950f38
Yanamoe <moritz.horch@web.de>
2019-04-30 10:51:01 +0200

    Moved amountFor to Rental - fits better here!

--- src/main/Rental.java
+++ src/main/Rental.java
@@ -13,4 +13,27 @@ class Rental {
     public Movie getMovie() {
         return movie;
     }
+
+    private double getCharge() {
+        double charge = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                charge += 2;
+                if (each.getDaysRented() > 2)
+                    charge += (each.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                charge += each.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                charge += 1.5;
+                if (each.getDaysRented() > 3)
+                    charge += (each.getDaysRented() - 3) * 1.5;
+                break;
+            default:
+                charge = 0;
+                break;
+        }
+        return charge;
+    }
 }
